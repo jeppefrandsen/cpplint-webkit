47066cd852fe873e744f656d1cda024b6fd57ef0
Jeppe Frandsen <jeppe@algrens.dk>
2024-05-29 08:21:10 +0000

    Renamed script to use default cpplint path

--- install.sh
+++ install.sh
@@ -2,6 +2,6 @@
 
 sudo cp -r cpplint-webkit/WebKit /usr/share
 sudo cp cpplint-webkit/check-webkit-style /usr/bin
-sudo cp cpplint-webkit/cpplint-webkit /usr/bin
+sudo cp cpplint-webkit/cpplint /usr/local/bin
 sudo git init /usr/share/WebKit
 sudo git config --global --add safe.directory /usr/share/WebKit
